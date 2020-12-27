b479905a7a1b46c11ebcd26ab3da0a466856bb27
halpert-keren <halpertkeren@gmail.com>
2020-12-27 22:04:13 +0200

    client sive and auth

--- server.js
+++ server.js
@@ -6,17 +6,26 @@ const PORT = process.env.PORT || 3000;
 const taskRouter    = require("./routers/task.router");
 const userRouter    = require("./routers/user.router");
 const messageRouter = require("./routers/message.router");
+const authentication= require("./routers/authentication.router");
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.set('Content-Type', 'application/json');
+    next();
+});
 
 app.use(logger("dev"));        
 
 app.use('/api/tasks', taskRouter.router);  
 app.use('/api/users', userRouter.router);
 app.use('/api/messages', messageRouter.router);
-
+app.use('/api/login', authentication.router);  
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
