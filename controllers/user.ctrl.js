b6d5ce403b382afa31e1824ea5408d3bf5901339
Danityec <Danityec@gmail.com>
2020-12-30 20:28:32 +0200

    update get email

--- controllers/user.ctrl.js
+++ controllers/user.ctrl.js
@@ -1,15 +1,22 @@
 const User = require('../models/user');
 
 getUsers = (req, res) => {
-    User.find({})
-    .then(docs => res.json(docs))
-    .catch(err => console.log(err))
+    if (req.query.email) {
+        User.findOne({ email: req.query.email })
+            .then(docs => res.json(docs))
+            .catch(err => console.log(err))
+    }
+    else {
+        User.find({})
+        .then(docs => res.json(docs))
+        .catch(err => console.log(err))
+    }
 }
 
 getUser = (req, res) => {
-    User.findOne({_id: req.params.id })
-    .then(docs => res.json(docs))
-    .catch(err => console.log(err))
+    User.findOne({ _id: req.params.id })
+        .then(docs => res.json(docs))
+        .catch(err => console.log(err))
 }
 
 createUser = (req, res) => {
@@ -21,11 +28,11 @@ createUser = (req, res) => {
     user.admin = body.admin
 
     user.save()
-        .then(() => res.json({_id: `${user.id}`}))
+        .then(() => res.json({ _id: `${user.id}` }))
         .catch(err => console.log(err))
 }
 
-updateUser = (req, res) => { 
+updateUser = (req, res) => {
     const { body } = req
     const user = {};
     user.firstName = body.firstName
@@ -33,23 +40,23 @@ updateUser = (req, res) => {
     user.email = body.email
     user.admin = body.admin
 
-    const query = {_id: req.params.id}
+    const query = { _id: req.params.id }
 
     User.updateOne(query, user)
-        .then(() => res.json({_id:`${req.params.id}`}))
+        .then(() => res.json({ _id: `${req.params.id}` }))
         .catch(err => console.log(err))
 }
 
-deleteUser = (req, res) => {     
-    User.deleteOne({_id: req.params.id})
-        .then(() => res.json({_id:`${req.params.id}`}))
+deleteUser = (req, res) => {
+    User.deleteOne({ _id: req.params.id })
+        .then(() => res.json({ _id: `${req.params.id}` }))
         .catch(err => console.log(err))
 }
 
-module.exports = { 
-    getUsers, 
-    getUser, 
-    createUser, 
-    updateUser, 
+module.exports = {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
     deleteUser
 }
