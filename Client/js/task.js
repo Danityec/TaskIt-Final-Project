f0256f47e1535d164519a63a6daaf773340a13d9
halpert-keren <halpertkeren@gmail.com>
2020-12-30 20:09:51 +0200

    task CRUD complete

--- Client/js/task.js
+++ Client/js/task.js
@@ -19,24 +19,32 @@ function getSingleTask() {
 function getShareList(task) {
     shareList = []
     shareArray = task['share']
-    const bar = new Promise((resolve) => {
-        shareArray.forEach(share => {
-            $.ajax({
-                url: `http://127.0.0.1:3000/api/users/${share}`,
-                type: 'GET',
-                success: function (user) {
-                    shareList.push(user['firstName'] + ' ' + user['lastName'])
-                    resolve()
-                }
+    console.log("shareArray: ", shareArray)
+    if (shareArray.length != 0) {
+        const bar = new Promise((resolve) => {
+            shareArray.forEach(share => {
+                $.ajax({
+                    url: `http://127.0.0.1:3000/api/users/${share}`,
+                    type: 'GET',
+                    success: function (user) {
+                        shareList.push(user['firstName'] + ' ' + user['lastName'])
+                        resolve()
+                    }
+                });
             });
         });
-    });
-    bar.then(() => {
+        bar.then(() => {
+            displaySingleTasks(task, shareList)
+        });
+    }
+    else
         displaySingleTasks(task, shareList)
-    });
 }
 
 function displaySingleTasks(task, shareList) {
+    $("#dynamic-task-name-header").empty();
+    $("#dynamic-task-name-header").append('<h2><b>' + task['name'] + '</b></h2>');
+
     $("#dynamic-task-name").empty();
     $("#dynamic-task-name").append('<h3>' + task['name'] + '</h3>');
 
@@ -65,7 +73,7 @@ function displaySingleTasks(task, shareList) {
             $("#dynamic-subtask-list").append(
                 '<tr class="row"><td class="col">' + subtask['name'] +
                 '</td><td class="col">' +
-                '<input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"' + status_string + '></input>' +
+                '<input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"' + status_string + ' onclick="updateSubaskStatus(\'' + subtask._id + '\',\'' + status_string + '\')"></input>' +
                 '</td><td class="col">' +
                 '<button class="edit" type="button" data-toggle="tooltip" data-bs-toggle="modal" data-bs-target="#edit-subtask"><i class="material-icons">&#xE254;</i></button>' +
                 '<button class="delete" type="button" data-toggle="tooltip"  data-bs-toggle="modal" data-bs-target="#delete-subtask"><i class="material-icons">&#xE872;</i></button>' +
@@ -100,23 +108,52 @@ function userAutocomplete() {
 }
 
 
+function updateSubaskStatus(subtask_id, checked) {
+    task_id = localStorage.getItem('task_id')
+    status = true
+    if (checked == 'checked')
+        status = false
+    updated_task = { "status": status }
+    $.ajax({
+        url: `http://127.0.0.1:3000/api/tasks/subtasks/${task_id}/${subtask_id}`,
+        type: 'PUT',
+        data: updated_task,
+        success: function () {
+            location.replace('dashboard.html');
+        }
+    });
+}
+
+
+
+
 function listeners() {
     $("#submit_edit_task").click(() => {
         task_id = localStorage.getItem('task_id')
         new_name = ''
         new_category = ''
-        new_share = []
+        new_share = ''
+        share_list = []
         if ($("#task_name_edit").val())
             new_name = $("#task_name_edit").val()
         if ($("#task_category_edit").val())
             new_category = $("#task_category_edit").val()
         if ($("#task_share").val())
-            new_share.push($("#task_share").val())
+            new_share = ($("#task_share").val())
 
+        if (new_share != '') {
+            $.ajax({
+                url: `http://127.0.0.1:3000/api/users/?email=${new_share}`,
+                type: 'GET',
+                success: function (user) {
+                    share_list.push(user._id)
+                }
+            });
+        }
         updated_task = {
             "name": new_name,
             "category": new_category,
-            "share": new_share
+            "share": share_list
         }
         $.ajax({
             url: `http://127.0.0.1:3000/api/tasks/${task_id}`,
@@ -144,15 +181,15 @@ function listeners() {
     $("#submit_create_subtask").click(() => {
         task_id = localStorage.getItem('task_id')
         new_subtask = {
-            "name": $("#task_name_create").val(),
-            "status": false,
+            "name": $("#task_name_create").val()
         }
+        console.log(task_id, " ", new_subtask)
         $.ajax({
             url: `http://127.0.0.1:3000/api/tasks/subtasks/${task_id}`,
             type: 'POST',
             data: new_subtask,
             success: function (data) {
-                getSingleTask()
+                location.replace("task.html");
             }
         });
     });
