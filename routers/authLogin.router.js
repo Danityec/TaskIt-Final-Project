ab34a99115856333e17c150eb8893104c03fcdeb
Danityec <Danityec@gmail.com>
2021-02-05 17:48:40 +0200

    update method token

--- routers/authLogin.router.js
+++ routers/authLogin.router.js
@@ -7,26 +7,26 @@ const authLoginController = require('../controllers/authLogin.ctrl');
 
 // router.get('/', authLoginController.getAuthLogin);
 
-// router.post('/', (req, res) => {
-//     let token = req.body.token
-//     async function verify() {
-//         const ticket = await client.verifyIdToken({
-//             idToken: token,
-//             audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-//             // Or, if multiple clients access the backend:
-//             //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-//         });
-//         const payload = ticket.getPayload();
-//         console.log(payload)
-//         const userid = payload['sub'];
-//         // If request specified a G Suite domain:
-//         // const domain = payload['hd'];
-//         res.json(payload)
+router.post('/', (req, res) => {
+    let token = req.body.token
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        console.log(payload)
+        const userid = payload['sub'];
+        // If request specified a G Suite domain:
+        // const domain = payload['hd'];
+        res.json(payload)
 
-//       }
-//       verify().catch(console.error);
+      }
+      verify().catch(console.error);
      
-// });
+});
 
 
 
