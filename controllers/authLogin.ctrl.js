682d5993d62642c8a2e104a679c4147762a5e5c9
Danityec <Danityec@gmail.com>
2021-02-06 13:06:52 +0200

    getLogout controller

--- controllers/authLogin.ctrl.js
+++ controllers/authLogin.ctrl.js
@@ -3,17 +3,17 @@ const UserCtrl = require('./user.ctrl')
 const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
-// getLogout = (req, res) => {
-//     console.log("logout1: ",req.session)
+getLogout = (req, res) => {
+    console.log("logout1: ",req.session)
 
-//     req.session.destroy((err) => {
-//         if(err){
-//             return console.log(err);
-//         }
-//         console.log("logout2: ",req.session)
-//         res.send("logout");
-//     });
-// };
+    req.session.destroy((err) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log("logout2: ",req.session)
+        res.send("logout");
+    });
+};
 
 verify = async (token) => {
     const ticket = await client.verifyIdToken({
