dced6d732dfb1200a26397e82602e672ac3bb462
Danityec <Danityec@gmail.com>
2021-02-17 13:18:41 +0200

    remove method update and delete from user

--- controllers/user.ctrl.js
+++ controllers/user.ctrl.js
@@ -32,7 +32,6 @@ createUser = (token, req, res) => {
         .then(() => {
             User.findOne({googleID: token['id']})
                 .then(docs => {
-                    res.cookie('user', docs)
                     res.json(docs)
                 })
                 .catch(err => console.log(err))
@@ -40,45 +39,8 @@ createUser = (token, req, res) => {
         .catch(err => console.log(err))
 }
 
-updateUser = (req, res) => {
-    const {body} = req
-    const user = {};
-
-    if (body.firstName == null || body.firstName == "") {
-        res.sendStatus(400)
-    } else {
-        user.firstName = body.firstName
-    }
-
-    if (body.lastName == null || body.lastName == "") {
-        res.sendStatus(400)
-    } else {
-        user.lastName = body.lastName
-    }
-
-    if (body.email == null || body.email == "") {
-        res.sendStatus(400)
-    } else {
-        user.email = body.email
-    }
-
-    user.admin = body.admin
-
-    User.updateOne({googleID: req.params.id}, user)
-        .then(() => res.json({googleID: `${req.params.id}`}))
-        .catch(err => console.log(err))
-}
-
-deleteUser = (req, res) => {
-    User.deleteOne({googleID: req.params.id})
-        .then(() => res.sendStatus(200))
-        .catch(err => console.log(err))
-}
-
 module.exports = {
     getUsers,
     getUser,
     createUser,
-    updateUser,
-    deleteUser,
 }
