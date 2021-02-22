533f18dd2195b6f77eb29897892c3b4ae336c66c
halpert-keren <halpertkeren@gmail.com>
2021-02-22 10:20:28 +0200

    cookie domains

--- models/session.js
+++ models/session.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 const User = require('../models/user');
 
 const sessionSchema = new Schema({
-    cookie: { type: User }
+    cookie: { type: Object }
 }, { collection: 'sessions' });
 
 const Session = model('Session', sessionSchema);
