0370556dd1e9ddde9637dc17110003e57b348ca2
halpert-keren <halpertkeren@gmail.com>
2021-02-22 09:47:00 +0200

    cookie domains

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,7 +1,7 @@
 const checkAuthenticated = (req, res, next) => {
     console.log('checkAuthenticated req:')
-    console.log(req)
-    if (req.session.user) {
+    console.log(req.cookies.user)
+    if (req.cookies.user) {
         next()
     } else {
         res.status(401).send('user is unauthenticated')
