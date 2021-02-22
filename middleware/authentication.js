45a3717793332caab9eb10bced6d0dcd1f5d55ac
halpert-keren <halpertkeren@gmail.com>
2021-02-22 11:44:04 +0200

    auth sessions

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,13 +1,11 @@
 const Session = require('../models/session');
 
 const checkAuthenticated = (req, res, next) => {
-    console.log("dddd")
+    console.log('checkAuthenticated')
     console.log(req.headers.user)
-
     Session.findOne({ id: req.headers.user })
         .then(docs => {
             console.log('checkAuthenticated = good!!')
-            console.log(docs)
             if (docs !== [])
                 next()
             else
